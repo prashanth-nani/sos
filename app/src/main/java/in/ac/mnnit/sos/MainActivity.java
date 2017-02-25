1f4ffe481d68d20f0aaaab86d694f85073c80d2d
Prashanth <prashanth1395@gmail.com>
2017-02-25 19:50:31 +0530

    Implement card layout for emergency contact list

--- app/src/main/java/in/ac/mnnit/sos/MainActivity.java
+++ app/src/main/java/in/ac/mnnit/sos/MainActivity.java
@@ -1,6 +1,5 @@
 package in.ac.mnnit.sos;
 
-import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
@@ -10,7 +9,6 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
-import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
